a4fc0ce0d96e3a04a983ac9da22f9bd4a45423a3
Anna Dinh <Annathuydinh@gmail.com>
2020-10-26 13:44:54 -0700

    set up server using express

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Connected')
+})
+
+app.use('/', express.static(path.join(__dirname, '/../client/dist')))
+
+app.listen(port, () => {
+  console.log(`Burger Practice app listening at http://localhost:${port}`)
+})
