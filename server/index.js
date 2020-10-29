0b193c13d0f5cfcfa82e89c7014a95721a99ddff
Anna Dinh <Annathuydinh@gmail.com>
2020-10-29 11:23:28 -0700

    removed .get to '/' pathway

--- server/index.js
+++ server/index.js
@@ -12,10 +12,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Connected')
-})
-
 app.use('/', express.static(path.join(__dirname, '/../client/dist')))
 
 app.listen(port, () => {
