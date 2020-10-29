f378d7e20adb81a37e3025074fce34828c52fd87
Anna Dinh <Annathuydinh@gmail.com>
2020-10-29 13:05:05 -0700

    Added transformedIngredients variable that will map over the Object's keys of props.ingredients

--- client/src/components/Burger/Burger.jsx
+++ client/src/components/Burger/Burger.jsx
@@ -3,11 +3,25 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
+  // transform an object to an array of keys
+  // Object.keys(props.ingredients) => ['salad', 'bacon, 'cheese', 'meat']
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])]
+    .map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+  // console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
