ef2aa19108983f27425734690147135037c685af
Anna Dinh <Annathuydinh@gmail.com>
2020-10-29 13:10:30 -0700

    imported BuildControls and added the BuildControls tag to be rendered

--- client/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ client/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux/Aux";
 import Burger from "../../components/Burger/Burger.jsx";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls.jsx";
 
 class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+      }
+    }
+  }
   render() {
     return (
       <div>
         <Aux>
-          <Burger />
-          <div>Burger Controls</div>
+          <Burger ingredients={this.state.ingredients}/>
+          <BuildControls />
         </Aux>
       </div>
     );
