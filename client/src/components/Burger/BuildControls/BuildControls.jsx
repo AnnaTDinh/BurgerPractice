58671efcafc502e701ce7e52abb218df413807fd
Anna Dinh <Annathuydinh@gmail.com>
2020-10-30 17:14:38 -0700

    Added build controls

--- client/src/components/Burger/BuildControls/BuildControls.jsx
+++ client/src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import classes from './buildControls.module.css';
+import React from "react";
+import classes from "./buildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl.jsx";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
 
 const buildControls = (props) => {
   return (
-    <div>
-
+    <div className={classes.BuildControls}>
+      {controls.map((control) => {
+        return <BuildControl key={control.label} label={control.label} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default buildControls;
